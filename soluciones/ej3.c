9e8655d4657d5de07a3031a65c04f0416e60fdf6
pamelamontiel <pamelamontielm@gmail.com>
2026-02-24 22:08:14 -0600

    Update ej3.c

--- soluciones/ej3.c
+++ soluciones/ej3.c
@@ -1,6 +1,28 @@
-
 #include <stdio.h>
+//Ejercicio 3
+
 int main(){
-    // Resolver ejercicio 3
+    int a[3];
+    int b[3];
+    int c[3];
+
+    for (int i=0; i<3; i++)
+    {
+        printf("La direccion de memoria de a[%i] es %p\n", i, &a[i]);
+    }
+    printf("\n");
+
+    for (int i=0; i<3; i++)
+    {
+        printf("La direccion de memoria de b[%i] es %p\n", i, &b[i]);
+    }
+    printf("\n");
+
+    for (int i=0; i<3; i++)
+    {
+        printf("La direccion de memoria de c[%i] es %p\n", i, &c[i]);
+    }
+    printf("\n");
+
     return 0;
 }
