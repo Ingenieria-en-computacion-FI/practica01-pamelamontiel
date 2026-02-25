84f1252fe6e862aa8245763bae854832c05679d0
pamelamontiel <pamelamontielm@gmail.com>
2026-02-24 22:36:15 -0600

    Update ej10.c

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -1,6 +1,12 @@
 
 #include <stdio.h>
 int main(){
-    // Resolver ejercicio 10
+    // Ejercicio 10
+    int gigante[1000000];
+
     return 0;
 }
+
+// ¿Por que falla? El arreglo intenta reservar 40 MB y la capacidad maxima de la Pila(Stack) es de 8MB
+//¿Donde se almacena? Toda variable declarada en main (), se guarda en la pila (Stack)
+// ¿Solución? Declararla como una variable global
