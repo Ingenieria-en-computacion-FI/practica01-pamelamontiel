d0c393f7b539755ee771945da9c07b107b396592
pamelamontiel <pamelamontielm@gmail.com>
2026-02-24 22:20:41 -0600

    Update ej8.c

--- soluciones/ej8.c
+++ soluciones/ej8.c
@@ -1,6 +1,8 @@
-
-#include <stdio.h>
-int main(){
-    // Resolver ejercicio 8
-    return 0;
-}
+// Ejercicio 8
+//base = 2000, sizeof(int)=4, m[1][2] = 2024
+//2024=2000+(Columnas+2)x4 => 2024=2000+(Columnas+2)x4 => 24= 4 Columnas+8 => 24-8=4 Columnas => 16/4= Columnas => 4= Columnas
+//número de columnas: 4
+//Hay 2 filas, 4 columnas y el tamaño de un entero de 4 bytes
+// 2x4x2 =32
+// tamaño total: 32 bytes 
+    
